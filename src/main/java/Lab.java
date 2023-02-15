92a5987259881e2135a1f54fe3e6a36124463a43
tuantrinh18 <tuantrinh1803@gmail.com>
2023-02-15 21:19:44 +0000

    LabDay4Completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -24,7 +24,14 @@ public class Lab {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
-        return false;
+        if(a == b)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     /**
@@ -33,7 +40,14 @@ public class Lab {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
-        return false;
+        if(a > b)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     /**
      * @param a the first number to be compared.
@@ -41,7 +55,14 @@ public class Lab {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
-        return false;
+        if(a < b)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     /**
      * @param a the first number to be compared.
@@ -49,7 +70,14 @@ public class Lab {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
-        return false;
+        if(a >= b)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     /**
      * @param a the first number to be compared.
@@ -57,7 +85,14 @@ public class Lab {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
-        return false;
+        if(a <= b)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     /**
@@ -66,6 +101,13 @@ public class Lab {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
-        return false;
+        if(a !=b)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
